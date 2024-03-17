404fcda168b740e4fe6c783cb94ef4ce02aae415
juchanhwang <ghkdwncks96@gmail.com>
2024-03-17 23:09:15 +0900

    feat(design-system/forwardRefWithAs): 린트 에러 수정 - any 타입 => never 타입으로 수정

--- packages/design-system/src/utils/forwardRefWithAs.tsx
+++ packages/design-system/src/utils/forwardRefWithAs.tsx
@@ -1,10 +1,16 @@
 import { forwardRef } from 'react';
 
+/**
+ * This is a hack, but basically we want to keep the full 'API' of the component, but we do want to
+ * wrap it in a forwardRef so that we _can_ passthrough the ref
+ * [래퍼런스 코드](https://github.com/tailwindlabs/headlessui/blob/main/packages/%40headlessui-react/src/utils/render.ts)
+ */
+
 export const forwardRefWithAs = <
   T extends { name: string; displayName?: string },
 >(
   component: T,
 ): T & { displayName: string } =>
-  Object.assign(forwardRef(component as unknown as any) as any, {
+  Object.assign(forwardRef(component as unknown as never) as never, {
     displayName: component.displayName ?? component.name,
   });
